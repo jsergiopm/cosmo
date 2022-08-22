36c58b61e00c2632252246ce296666dd80043976
jsergiopm <igomez85803@umanizales.edu.co>
2022-08-22 17:23:27 -0500

    Inventario cargado

--- cosmo.py
+++ cosmo.py
@@ -1,9 +1,13 @@
+from cgitb import text
 from re import T
 import PySimpleGUI as sg
 import csv
 import shutil
 import os
 from datetime import datetime
+import pandas as pd
+from openpyxl import *
+
 
 
 def read_csv(filename):
@@ -20,85 +24,9 @@ def read_csv(filename):
             i += 1 
         return old_name_list, new_name_list
 
-def write_log(message):
-    f = open("log.txt", "a")
-    current_datetime = datetime.now()
-    f.write(str(current_datetime) + '\n' + message)
-    f.close()
-
-
-def find_item(HL, item):
-    for i in range(len(HL)):
-        if HL[i] == item: 
-            return i 
-
-
-def iterative_file_name(file):
-    if file.find("-") != -1:
-        if file.find("-1") != -1:
-            file = file.replace("-1", "-2")
-        elif file.find("-2") != -1:
-            file = file.replace("-2", "-3")
-        elif file.find("-3") != -1:
-            file = file.replace("-3", "-4")
-        elif file.find("-4") != -1:
-            file = file.replace("-4", "-5")
-        elif file.find("-5") != -1:
-            file = file.replace("-5", "-6")
-    else:
-        file = file.replace(".pdf", "-1.pdf")
-    return file
-
-
-def move_file(file_name, folder_names, status):
-    log = ""
-    only_files = next(os.walk('D:/Activos/Prestaciones_recuperadas/'))[2] 
-    none_list = []
-    for file in only_files:
-        file_without_index = file.replace("PDF", "pdf")
-        file = file.replace("PDF", "pdf")
-        file_without_index = file.replace("-1", "")
-        file_without_index = file_without_index.replace("-2", "")
-        file_without_index = file_without_index.replace("-3", "")
-        file_without_index = file_without_index.replace("-4", "")
-        file_without_index = file_without_index.replace("-5", "")
-        #source = 'C:/Users/Tesoreria/Desktop/Prueba/origen/' + file
-        source = 'D:/Activos/Prestaciones_recuperadas/' + file
-        index = find_item(file_name, file_without_index)
-        if index == None:
-            none_list.append(file)
-            continue
-        if status[index].strip() == "ACTIVOS":
-            destination = 'D:/CARPETAS/ACTIVOS/' + folder_names[index] + '/PrestacionSocial/' + file
-        elif status[index] == "INACTIVOS":
-            destination = 'D:/CARPETAS/INACTIVOS/' + folder_names[index] + '/PrestacionSocial/' + file
-        if os.path.exists(destination):
-            file = iterative_file_name(file)
-            print("El archivo ya se encuentra en el destino, el nombre del archivo ahora es", file)
-            #log += "El archivo "+ file + " ya se encuentra en el destino \n"
-            #continue
-        try:
-            shutil.move(source, destination)
-        except FileNotFoundError:
-            print('El archivo', file, 'no fue encontrado, revise la ruta')
-            log += 'El archivo ' + file + ' no fue encontrado, revise la ruta\n'
-            continue
-        print("Moved: " + file)
-        log += "Moved: " + file + "\n"
-    if none_list != []:
-        print(len(none_list) , "elementos no fueron encontrados en el inventario: ")
-        print(none_list)
-    log += str(len(none_list)) + " elementos no fueron encontrados en el inventario: \n"
-    for none_item in none_list:
-        log += none_item 
-        log += "\n"
-    write_log(log + '\n')
-
 
 def change_filename(list_of_files, old_names, new_names):
     new_list = []
-    print(old_names)
-    print(new_names)
     for file in list_of_files:
         try:
             index = old_names.index(file)
@@ -128,13 +56,13 @@ def run():
     main_window = sg.Window('Cosmo', layout, size=(330,150))
     #window["OLD-FILENAMES-LIST"].hide()
 
-    change_window_active = False
+
     while True:
         event, values = main_window.read()
         if event == sg.WIN_CLOSED or event=="Exit":
             break
         elif event == "Cambiar":
-            change_window_active = True
+
             main_window.Close()
             layout2 = [
             [
@@ -147,11 +75,11 @@ def run():
             ],
             [
                 sg.Text("Seleccionar inventario: "), 
-                sg.Input(), sg.FileBrowse(key="-FILE-", file_types=(('CSV FILES'), ('*.csv')), button_text="Seleccionar")
+                sg.Input(), sg.FileBrowse(key="-FILE-", file_types=(('CSV FILES', '*.csv'),), button_text="Seleccionar")
             ],
             [
-                sg.Listbox(values=[], enable_events=True, size=(40, 20), key="OLD-FILENAMES-LIST"),
-                sg.Listbox(values=[], enable_events=True, size=(40, 20), key="NEW-FILENAMES-LIST")
+                sg.Listbox(values=[], enable_events=True, size=(40, 10), key="OLD-FILENAMES-LIST", visible = False),
+                sg.Listbox(values=[], enable_events=True, size=(40, 10), key="NEW-FILENAMES-LIST",  visible = False)
             ],
             [
             sg.Button("Ver"),
@@ -159,14 +87,12 @@ def run():
             ], 
             ]
             
-            change_window = sg.Window('Cosmo | Cambiar nombre', layout2)
+            change_filenames_window = sg.Window('Cosmo | Cambiar nombre', layout2)
             while True:
-                event2, values2 = change_window.read()
+                event2, values2 = change_filenames_window.read()
                     
                 if event2 == sg.WIN_CLOSED or event2 == 'Exit':
-                    change_window.Close()
-                    window_2_active = False
-                    #main_window.UnHide()
+                    change_filenames_window.Close()
                     break
                 folder = values2["FolderBrowse"]
                 try:
@@ -181,23 +107,26 @@ def run():
                     if os.path.isfile(os.path.join(folder, f))
                     and f.lower().endswith((".pdf"))
                 ]
-                change_window["OLD-FILENAMES-LIST"].update(fnames)
-                csv_file = values2["-FILE-"]
-                old_names, new_names = read_csv(csv_file)
-                list_of_new_names = change_filename(file_list, old_names, new_names)
-                print(list_of_new_names)
-                change_window["NEW-FILENAMES-LIST"].update(list_of_new_names)
+
+                if event2 == "Ver":
+                    change_filenames_window["OLD-FILENAMES-LIST"].update(visible=True)
+                    change_filenames_window["NEW-FILENAMES-LIST"].update(visible=True)
+                    change_filenames_window["OLD-FILENAMES-LIST"].update(fnames)
+                    csv_file = values2["-FILE-"]
+                    old_names, new_names = read_csv(csv_file)
+                    list_of_new_names = change_filename(file_list, old_names, new_names)
+                    change_filenames_window["NEW-FILENAMES-LIST"].update(list_of_new_names)
                 if event2 == "Comenzar":
-                    change_window.close()
+                    change_filenames_window.close()
                     BAR_MAX =  len(file_list)
                     layout3 = [[sg.Text('Cambiando nombres...')],
-                    [sg.ProgressBar(BAR_MAX, orientation='h', size=(20,20), key='-PROG-')],
-                    [sg.Cancel()]]
+                        [sg.ProgressBar(BAR_MAX, orientation='h', size=(20,20), key='-PROG-')],
+                        [sg.Cancel()]]
 
                     change_progress_window = sg.Window('Cambiar nombre de los atchivos', layout3)
 
                     for i in range(len(file_list)):
-                        event, values = change_progress_window.read(timeout=10)
+                        event = change_progress_window.read(timeout=10)
                         if event == 'Cancel' or event == sg.WIN_CLOSED:
                             break
                         source = os.path.join(folder,file_list[i])
@@ -207,6 +136,63 @@ def run():
                     change_progress_window.close()
 
                     sg.popup('Tarea completada con éxito')
+
+        elif event == "Mover archivos":
+            main_window.close()
+            layout4 =[
+            [
+                sg.T("")
+            ],
+            [sg.Text('Mover archivos másivamente', justification='center', font=("Helvetica", 25), relief=sg.RELIEF_RIDGE)],
+            [
+                sg.Text("Seleccionar inventario: "), 
+                sg.Input(), sg.FileBrowse(key="excel", file_types=(('XLSX FILES','*.xlsx'),('XLS FILES','*.xls')), button_text="Seleccionar")
+            ],
+            [
+                sg.Listbox(values=[], enable_events=True, size=(40, 10), key="OLD-FILENAMES-LIST", visible = False),
+            ],
+            [
+            sg.Button("Cargar inventario")
+            ], 
+            ]
+
+            move_files_window = sg.Window('Cosmo | Mover archivos', layout4)
+            while True:
+                event4, values4 = move_files_window.read()
+                if event4 == sg.WIN_CLOSED or event4 == 'Exit':
+                    move_files_window.Close()
+                    break
+                excel = values4["excel"]
+                data_from_excel = pd.read_html(excel)
+                if event4 == "Cargar inventario":
+                    move_files_window.close()
+                    print(data_from_excel[2][3])
+                    headings = [str(data_from_excel[0][x])+' ..' for x in range(len(data_from_excel[0]))]
+                    layout5 = [
+                        [sg.Text('Cambiar nombre de archivos másivamente', justification='center')]
+                        [sg.Table(values=data_from_excel, 
+                        headings=headings, 
+                        max_col_width=35,
+                        auto_size_columns=True,
+                        display_row_numbers=True,
+                        justification='right',
+                        num_rows=10,
+                        key='-TABLE-',
+                        row_height=35)]
+                    ]
+
+                    table_window = sg.Window("Información del inventario", layout5)
+                    while True:
+                        event5, values = table_window.read()
+                        if event5 == "Exit" or event == sg.WIN_CLOSED:
+                            break
+                            
+                    table_window.close()
+
+
+                
+                
+
 
 if __name__ == "__main__":
     run()
